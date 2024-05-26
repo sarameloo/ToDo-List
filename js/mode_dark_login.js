b0d062be1387e932e9721ad12acf250229a45338
brê_Garcia <brennol3vy@gmail.com>
2024-05-25 22:04:40 -0300

    dark mode semi-completo

--- js/mode_dark_login.js
+++ js/mode_dark_login.js
@@ -1,19 +1,16 @@
 //Pegando elementos
 const chk = document.querySelector('#chk');
+const label = document.querySelector('.label');
 const header = document.querySelector('.header');
 const card = document.querySelector('.card');
-const h2 = document.querySelector('.card>h2');
-const input = document.querySelectorAll('.dados');
-const botao = document.querySelector('.botao');
 const link = document.querySelector('.link');
 
 //Trocando as cores
 
 chk.addEventListener('change', ()=>{
     document.body.classList.toggle('dark');
+    label.classList.toggle('dark-label');
     header.classList.toggle('dark-header');
     card.classList.toggle('dark-card');
-    h2.classList.toggle('dark-h2');
-    input[0].classList.toggle('dark-input');
-    input[1].classList.toggle('dark-input');
+    link.classList.toggle('dark-link');
 })
