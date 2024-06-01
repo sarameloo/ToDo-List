bc8d53caf635774f47b3e366c5958135e9c6e8c0
brê_Garcia <brennol3vy@gmail.com>
2024-05-31 22:15:17 -0300

    conenctando conta v2

--- php/conectar_conta.php
+++ php/conectar_conta.php
@@ -4,18 +4,18 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Origin: *");
 
-include "conexao.php";
+include 'conexao.php';
 
 session_start();
 $email = $_SESSION['email'];
+$sql_nome = "select nome from usuario where = ?";
+$stmt = $conn->prepare($sql_nome);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$stmt->close();
 
-$sql_nome = "select nome from usuario where = '$email'";
-$resultado = $conn->query($sql_nome);
-
-$row = $resultado->fetch_assoc();
-
-$nome = $row['nome'];
-echo "Hello word"
+//$nome = $row['nome'];
+echo "Hello word";
 
 
 ?>
