7f5d665c5a3edcfed1874d337cf697d3e58c41e9
brê_Garcia <brennol3vy@gmail.com>
2024-06-02 00:31:52 -0300

    correção no contador de concluídas

--- js/excluir-concluir_task.js
+++ js/excluir-concluir_task.js
@@ -1,3 +1,4 @@
+const cont_concluidas = document.querySelector('.cont_concluidas');
 function deletar_concluir_task(){
     var tarefas = document.querySelectorAll('.task');
     tarefas.forEach(task=>{
@@ -26,6 +27,21 @@ function deletar_concluir_task(){
         })
         btn_concluir.addEventListener('click', (event)=>{
             event.preventDefault();
+            fetch('php/contadores.php', {
+                method: 'POST'
+            })
+            .then(response=>{
+                if(!response.ok){
+                    throw new Error('Falha na requisição');
+                }
+                return response.json();
+            })
+            .then(data=>{
+                cont_concluidas.textContent = data.concluidas;
+            })
+            .catch(error=>{
+                console.log(error);
+            })
             fetch('php/concluir_task.php', {
                 method: 'POST',
                 headers: {
