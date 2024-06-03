c04ed6a729060c42a4f6415971f1860c6ecc99f5
brê_Garcia <brennol3vy@gmail.com>
2024-06-02 23:03:46 -0300

    adicionando alguns comentários

--- js/excluir_conta.js
+++ js/excluir_conta.js
@@ -1,22 +1,22 @@
-const deletar_conta = document.querySelector('.excluir-conta');
+const deletar_conta = document.querySelector('.excluir-conta'); //pegando botão de excluir conta
 
-deletar_conta.addEventListener('click', (event)=>{
-    event.preventDefault();
-    var confirmacao = confirm('Deseja excluir sua conta?');
-    if (confirmacao){
-        fetch('php/excluir_conta.php',{
-            method: 'POST'
+deletar_conta.addEventListener('click', (event)=>{ //adicionando evento de click
+    event.preventDefault(); //previnindo evento padrão
+    var confirmacao = confirm('Deseja excluir sua conta?'); //alerta de confirmação
+    if (confirmacao){ //se o retorno for verdadeiro
+        fetch('php/excluir_conta.php',{ //fazendo requisição via fetch
+            method: 'POST' //método de envio POST
         })
-        .then((response)=>{
-            if (!response.ok){
-                throw new Error('Falha na requisição');
+        .then((response)=>{ //reposta de requisição
+            if (!response.ok){ //se a resposta não for 'ok'
+                throw new Error('Falha na requisição'); //mensagem de erro
             }
             else{
-                window.location.href = "login.html";
+                window.location.href = "login.html"; //redirecionando para o página de login
             }
         })
-        .catch((erro)=>{
-            console.log(erro);
+        .catch((erro)=>{ //tratando erros
+            console.log(erro); //imprimindo erros da requisição
         })
     }
 })
